4b6c7a9c945ebb79e361536a89b058b014e8b5dd
Rajatgit6 <66821886+Rajatgit6@users.noreply.github.com>
2021-08-14 21:37:22 +0530

    Update urls.py

--- iCoder/iCoder/urls.py
+++ iCoder/iCoder/urls.py
@@ -19,10 +19,11 @@ from home import views
 admin.site.site_header="iCoder Admin"
 admin.site.site_title="iCoder Admin Panel"
 admin.site.index_title="Welcome to iCoder Admin Panel"
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('', include('home.urls')),
     path('blog/', include('blog.urls')),
     
-]
+]+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
